24c0e4927ea2bb10d211c22e76ef8789c4b694ba
Zongming Liu <zongmingxliu@gmail.com>
2017-07-01 13:42:31 -0700

    Remove notes

--- cs221-code/lab1/q1.cc
+++ cs221-code/lab1/q1.cc
@@ -1,11 +1,12 @@
-// WARNING: This code will NOT compile.  
+// WARNING: This code will NOT compile.
 // It contains the code samples from the cs221 lab 1 write-up.
 
 #include <iostream>
 
-float circle_area(float radius);  // declare function before main()
+float circle_area(float radius); // declare function before main()
 
-int main(void) {
+int main(void)
+{
   float circle_radius;
   std::cout << "Enter radius:" << std::endl;
   std::cin >> circle_radius;
@@ -13,6 +14,7 @@ int main(void) {
   return 0;
 }
 
-float circle_area(float radius) {
-  return 3.14159 * radius * radius;  // = pi * r^2
+float circle_area(float radius)
+{
+  return 3.14159 * radius * radius; // = pi * r^2
 }
