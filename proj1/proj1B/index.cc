dc3a87e021438ed8071351b5f73ab72f406f3c24
Harding Liu <zongmingxliu@gmail.com>
2018-11-24 00:23:33 -0400

    Fix formatting

--- proj1/proj1B/index.cc
+++ proj1/proj1B/index.cc
@@ -1,13 +1,12 @@
 #include "skiplist.h"
 // Remove all characters except letters (A-Z,a-z) from line,
 // except keep '-' or '\'' if they are between letters.
-void lowercaseWords(string & line) {
-  for( string::iterator it = line.begin(); it != line.end(); ++it ) {
-    if( !isalpha(*it) ) {
-      if( (*it != '-' && *it != '\'') ||
-	  it == line.begin() || it+1 == line.end() ||
-	  !isalpha(*(it-1)) || !isalpha(*(it+1)) ) {
-	*it = ' ';
+void lowercaseWords(string &line) {
+  for (string::iterator it = line.begin(); it != line.end(); ++it) {
+    if (!isalpha(*it)) {
+      if ((*it != '-' && *it != '\'') || it == line.begin() ||
+          it + 1 == line.end() || !isalpha(*(it - 1)) || !isalpha(*(it + 1))) {
+        *it = ' ';
       }
     } else {
       *it = tolower(*it);
@@ -15,46 +14,43 @@ void lowercaseWords(string & line) {
   }
 }
 
-void print(ostream & output, string & word) {
-  output << word << endl;
-}
+void print(ostream &output, string &word) { output << word << endl; }
 
 int main(int argc, char *argv[]) {
-  if( argc != 3 ) {
+  if (argc != 3) {
     cerr << "Usage: " << argv[0] << " book.txt index.txt" << endl;
     exit(1);
   }
-    
+
   ifstream fin;
   fin.open(argv[1]);
-  if( !fin.is_open() ) {
+  if (!fin.is_open()) {
     cerr << "Unable to open " << argv[1] << endl;
     exit(2);
   }
-    
+
   ofstream fout;
   fout.open(argv[2]);
-  if( !fout.is_open() ) {
+  if (!fout.is_open()) {
     cerr << "Unable to open " << argv[2] << endl;
     exit(3);
   }
-  Node* head = new Node;
+  Node *head = new Node;
   head->height = MAX_HEIGHT;
   string line, word;
   int pagen, linen;
-  linen = 0; 
-  while( !fin.eof() ) {
-    getline(fin,line);
+  linen = 0;
+  while (!fin.eof()) {
+    getline(fin, line);
     linen += 1;
     if ((linen % 40) == 0) {
-      pagen = linen/40;
-    }
-    else {
-      pagen = (linen/40) + 1;
+      pagen = linen / 40;
+    } else {
+      pagen = (linen / 40) + 1;
     }
     lowercaseWords(line);
     istringstream iss(line, istringstream::in);
-    while( iss >> word ) {
+    while (iss >> word) {
       insert(word, pagen, head);
     }
   }
--- proj1/proj1B/skiplist.h
+++ proj1/proj1B/skiplist.h
@@ -1,24 +1,24 @@
-#include <iostream>
-#include <vector>
-#include <string>
 #include <cctype>
-#include <sstream>
-#include <fstream>
 #include <cstdlib>
 #include <ctime>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
 using namespace std;
 
 const int MAX_HEIGHT = 16;
 
-struct Node{
-    string entry;
-    int counts;
-    vector<int> pages;
-    int height;
-    Node* next[MAX_HEIGHT];
+struct Node {
+  string entry;
+  int counts;
+  vector<int> pages;
+  int height;
+  Node *next[MAX_HEIGHT];
 };
 
 int randBit(void);
-Node* find(string word, Node* head);
-void insert(string word, int pagei, Node* head);
-void printInOrder(ostream& output, Node* head);
+Node *find(string word, Node *head);
+void insert(string word, int pagei, Node *head);
+void printInOrder(ostream &output, Node *head);
