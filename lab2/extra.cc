7b6ad71d555f58866a62a280fff3cc4a03ea92a2
Harding Liu <zongmingxliu@gmail.com>
2018-11-25 01:32:49 -0400

    Fix formatting

--- lab2/extra.cc
+++ lab2/extra.cc
@@ -6,15 +6,13 @@ int a = 7;
 int b = 6;
 int *c = &b;
 
-void test(int &x, int y, int *&z)
-{
-    x++;
-    y++;
-    z = &a;
+void test(int &x, int y, int *&z) {
+  x++;
+  y++;
+  z = &a;
 }
-int main()
-{
-    test(a, b, c);
-    cout << a << " " << b << " " << *c << endl;
-    return 0;
+int main() {
+  test(a, b, c);
+  cout << a << " " << b << " " << *c << endl;
+  return 0;
 }
