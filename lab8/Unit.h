4d8a9d2fae35b00650fccb71e73e0d7185c552db
Harding Liu <zongmingxliu@gmail.com>
2018-11-25 22:21:53 -0400

    Fix formatting

--- lab8/Unit.h
+++ lab8/Unit.h
@@ -6,28 +6,29 @@
 
 class Unit {
 public:
-	Unit();
-	virtual ~Unit();
+  Unit();
+  virtual ~Unit();
 
-	void assertEquals(std::string msg, int expected, int actual);
-	void assert_LT(std::string msg, int lhs, int rhs);
-	void assert_LE(std::string msg, int lhs, int rhs);
-	void assertvectorEquals(std::string msg, std::vector<int>& expected, std::vector<int>& actual);
-	void assertNonNull(std::string msg, void* actual);
-	void assertNull(std::string msg, void* actual);
-    void assertTrue(std::string msg, bool actual); 
-	void printResults();
+  void assertEquals(std::string msg, int expected, int actual);
+  void assert_LT(std::string msg, int lhs, int rhs);
+  void assert_LE(std::string msg, int lhs, int rhs);
+  void assertvectorEquals(std::string msg, std::vector<int> &expected,
+                          std::vector<int> &actual);
+  void assertNonNull(std::string msg, void *actual);
+  void assertNull(std::string msg, void *actual);
+  void assertTrue(std::string msg, bool actual);
+  void printResults();
 
-	void insistNonNull(std::string msg, void* actual);
-	bool hasErrors( void ); 
-	bool isActive( void ); 
+  void insistNonNull(std::string msg, void *actual);
+  bool hasErrors(void);
+  bool isActive(void);
 
 private:
-	std::vector<std::string> failures;
+  std::vector<std::string> failures;
 
-	std::vector<std::string> successes;
+  std::vector<std::string> successes;
 
-	std::string toStr(std::vector<int>& v);
+  std::string toStr(std::vector<int> &v);
 };
 
 #endif /* UNIT_H_ */
